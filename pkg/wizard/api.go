252892f767af604991b72e83ada2ae84fdaf3b3b
Anton Zavodchikov <terisbackno@gmail.com>
2022-12-22 22:24:36 +0600

    Implement single item mint link

--- pkg/wizard/api.go
+++ pkg/wizard/api.go
@@ -8,7 +8,29 @@ import (
 )
 
 var BaseURL = "https://telegram-nft-wizard.vercel.app/"
+var SingleItemPath = "/createnft"
 var CollectionPath = "/createcollection"
+
+func SingleItemLink(fileID string) (string, error) {
+	if err := v.Validate.Var(fileID, "required"); err != nil {
+		return "", err
+	}
+
+	u, err := url.ParseRequestURI(BaseURL)
+	if err != nil {
+		return "", err
+	}
+
+	u = u.JoinPath(SingleItemPath)
+
+	// Build up URL Query
+	var query url.Values
+	query.Set("file_id", fileID)
+	u.RawQuery = query.Encode()
+
+	// Output URL
+	return u.String(), nil
+}
 
 type CollectionOptions struct {
 	Name    string `validate:"required"`
@@ -38,7 +60,12 @@ func CreateCollectionLink(options CollectionOptions) (string, error) {
 	}
 
 	for index, file_id := range options.FileIDs {
-		query.Set("file_id"+strconv.Itoa(index), file_id)
+		num := strconv.Itoa(index)
+		if index == 0 {
+			num = ""
+		}
+
+		query.Set("file_id"+num, file_id)
 	}
 
 	query.Set("item_count", strconv.Itoa(len(options.FileIDs)))
